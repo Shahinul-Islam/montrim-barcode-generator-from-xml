8ce4a4242aed2322f75fdeb95048dbf72913c468
shahinul islam <shahinul37@outlook.com>
2023-05-15 11:21:52 +0600

    layout dimension changed

--- src/components/UploadBtn/Layouts/Layout01.js
+++ src/components/UploadBtn/Layouts/Layout01.js
@@ -3,6 +3,7 @@ import Barcode from "react-barcode";
 import { BsCurrencyEuro } from "react-icons/bs";
 import { BiRfid } from "react-icons/bi";
 import { BsCircle } from "react-icons/bs";
+import { GrBottomCorner } from "react-icons/gr";
 import "../HangTag.css";
 import fscImage from "../../../images/fsc.png";
 
@@ -18,14 +19,14 @@ const Layout01 = ({ item }) => {
 		sellingPrice,
 	} = item;
 	return (
-		<div className="bg-white p-3 w-80 text-black mx-auto my-3 shadow-lg">
-			<div className="text-center relative py-1 -mx-1">
+		<div className="bg-white  text-black mx-auto my-3 shadow-lg h-[359.06px] w-[151.7px]">
+			<div className="text-center relative py-1 -mx-1 h-[310.06px]">
 				<div className="flex justify-around items-center">
 					<div className="flex flex-col justify-start">
-						<img src={fscImage} alt="" className="w-10" />
+						<img src={fscImage} alt="" className="w-4" />
 						<div
 							style={{
-								fontSize: "10px",
+								fontSize: "6px",
 								marginTop: "3px",
 								marginLeft: "5px",
 							}}
@@ -34,20 +35,25 @@ const Layout01 = ({ item }) => {
 							<p>FSC&copy; C111278</p>
 						</div>
 					</div>
-					<BsCircle className="text-3xl"></BsCircle>
-					<BiRfid className="text-3xl" />
+					<BsCircle className="text-lg"></BsCircle>
+					<BiRfid className="text-lg" />
 				</div>
 				<div className="primarySize">
 					<p>Size</p>
-					<p className="text-3xl">{primarySize}</p>
+					<p className="text-xl">{primarySize}</p>
 				</div>
 				<Barcode
 					value={`${barCode}`}
-					format="CODE128"
+					// value={"123456789012"}
+					format="EAN13"
 					displayValue={true}
-					width={1}
+					width={1.3}
+					height={40}
 				/>
-				<p className="text-lg seasonCode">{seasonCode}</p>
+				<p className="text-sm seasonCode">{seasonCode}</p>
+				<span>
+					<GrBottomCorner className="bottomCorner" />
+				</span>
 				<div>
 					<div
 						style={{
@@ -74,7 +80,7 @@ const Layout01 = ({ item }) => {
 
 			<div className="border-t-2 border-dotted mt-2">
 				<p className="my-1 font-bold flex items-center justify-center">
-					<BsCurrencyEuro /> <span className="text-3xl">{sellingPrice}</span>
+					<BsCurrencyEuro /> <span className="text-xl">{sellingPrice}</span>
 				</p>
 			</div>
 		</div>
